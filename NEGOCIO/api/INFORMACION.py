ff9a440e597ed310afe96e985caedaaa76289324
F3SHARITH <dahiana.bermudez2007@gmail.com>
2026-06-02 13:07:48 -0500

    correccion

--- NEGOCIO/api/INFORMACION.py
+++
@@ -1,8 +0,0 @@
-# Ascendente
-#GET /api/v1/bancos/?ordering=nombre_banco
-
-# Descendente (con guión)
-#GET /api/v1/bancos/?ordering=-nombre_banco
-
-# Por fecha
-#GET /api/v1/leads/?ordering=-fecha_generacion
